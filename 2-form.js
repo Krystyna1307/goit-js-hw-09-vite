83ace566cc14a0379419405a23f4994eb7b9928a
Krystyna1307 <132517899+Krystyna1307@users.noreply.github.com>
2024-09-12 17:04:17 +0000

    Deploying to gh-pages from @ Krystyna1307/goit-js-hw-09-vite@bf18b41554e64dbb72d9b50adf07071fa6fedf7d 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";console.log("Form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const a=document.querySelector(".feedback-form"),e={email:"",message:""},s="feedback-form-state",l=localStorage.getItem(s);l&&(Object.assign(e,JSON.parse(l)),a.email.value=e.email,a.message.value=e.message);a.addEventListener("input",t=>{e[t.target.name]=t.target.value,localStorage.setItem(s,JSON.stringify(e))});a.addEventListener("submit",t=>{if(t.preventDefault(),!e.email||!e.message){alert("Fill please all fields");return}console.log(e),localStorage.removeItem(s),a.reset(),e.email="",e.message=""});
 //# sourceMappingURL=2-form.js.map
